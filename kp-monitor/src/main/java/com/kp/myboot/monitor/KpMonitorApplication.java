45220ffd6014ae30aeff3540fb644bf2d35f2468
fanda521 <1056015243@qq.com>
2019-10-11 19:11:33 +0800

    在原来文件基础上修改的小文件

--- kp-monitor/src/main/java/com/kp/myboot/monitor/KpMonitorApplication.java
+++ kp-monitor/src/main/java/com/kp/myboot/monitor/KpMonitorApplication.java
@@ -3,9 +3,11 @@ package com.kp.myboot.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAdminServer //监听服务
+@EnableDiscoveryClient//开启服务发现注册
 public class KpMonitorApplication {
 
     public static void main(String[] args) {
